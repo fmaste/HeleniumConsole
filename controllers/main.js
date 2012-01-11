919f0702d5e4d1d0e5546aba8f096a9af406cd1d
fmaste <fmaste@users.noreply.github.com>
2012-01-11 18:51:54 -0300

    Move responsibility to the list controller

--- controllers/main.js
+++ controllers/main.js
@@ -3,32 +3,17 @@ jsMVC.controller.view.Class({
 	parentName: ""
 },function() {
 
-	var getTests = null;
-	var getTestEnvironments = null;
-
 	this.init = function () {
-		getTests = jQuery.ajax("/model/getTests.php");
-		getTestEnvironments = jQuery.ajax("/model/getTestEnvironments.php");
 	}
 
 	this.onLoad = function () {
-		getTests
-		.done(function (tests) {
-			getTestEnvironments.
-			done(function (environments) {
-				jsMVC.render(
-					"#content", 
-					"testsList", 
-					null,
-					"testsList", 
-					[tests, environments]
-				);
-			}).fail(function () {
-				alert("Failed to load the environments.");
-			});
-		}).fail(function () {
-			alert("Failed to load the tests.");
-		});
+		jsMVC.render(
+			"#content", 
+			"testsList", 
+			null,
+			"testsList", 
+			[]
+		);
 	}
 
 });
--- controllers/testsList.js
+++ controllers/testsList.js
@@ -3,18 +3,24 @@ jsMVC.controller.view.Class({
 	parentName: ""
 },function() {
 
-	var tests = null;
-	var environments = null;
+	this.getTests = null;
+	this.getTestEnvironments = null;
 
-	this.init = function (testsP, environmentsP) {
-		tests = testsP;
-		environments = environmentsP; 
+	this.init = function () {
+		this.getTests = jQuery.ajax("/model/getTests.php");
+		this.getTestEnvironments = jQuery.ajax("/model/getTestEnvironments.php");
 	}
 
 	this.onLoad = function () {
+		jQuery.when(this.getTests, this.getTestEnvironments).done(this.process);
+	}
+
+	this.process = function (tests, environments) {
+		tests = tests[0];
+		environments = environments[0];
 		var i = 0;
 		for (var key in tests) {
-			i++;
+ 			i++;
 			var test = tests[key];
 			var appended = jQuery('<li class="eachTest"></li>').
 				appendTo(jQuery("#testList", this.view));
@@ -24,7 +30,7 @@ jsMVC.controller.view.Class({
 			appended = jQuery('<ul class="eachRow"></ul>').
 				appendTo(appended);
 			jsMVC.render(appended, "test", null, "test", [test, environments]);
-		}
+                }
 	}
 
 });
