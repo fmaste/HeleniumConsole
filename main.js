74914b4012cba356f645d7aca5da89b59a985421
fmaste <fmaste@users.noreply.github.com>
2012-01-11 18:35:47 -0300

    Remove comment

--- main.js
+++ main.js
@@ -24,10 +24,7 @@ jsMVC.controller.application.Class({
 	}
 
 	this.onLoad = function () {
-		// Libs loaded, translations loaded, styles loaded, etc.
-		// Here I can dinamically add views to the application view! For example: decide to show a login view or a main view.
 		jsMVC.render(this.view, "main", null, "main");
 	}
 
 });
-
