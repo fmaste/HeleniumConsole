1478b048ba28925c989b2079c995ed8f6dfba84a
fmaste <fmaste@users.noreply.github.com>
2012-01-17 13:38:47 -0300

    Use pages controllers

---
+++ pages/main.js
@@ -0,0 +1,22 @@
+jsMVC.controller.page.Class({
+	className: "main"
+},function() {
+
+	this.init = function () {
+	}
+
+	this.getTitle = function () {
+		// This text will be used as page title.
+		return "Mastellonium";
+	}
+
+	this.getFavIcon = function () {
+		// The file name of the favicon image.
+		return "favicon.png";
+	}
+
+	this.onLoad = function () {
+		jsMVC.render(this.view, "main");
+	}
+
+});
